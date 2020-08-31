a388a99c71c6bbc2112a3896ae5f53c82012cd77
Tiffany Brand <brand2learn@gmail.com>
2020-08-31 09:16:36 -0400

    Initial Express setup with index route

---
+++ server.js
@@ -0,0 +1,28 @@
+// Dependencies
+// =============================================================
+const express = require('express');
+
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Sets up the Express app to serve static files
+app.use(express.static('public'))
+
+// ROUTER
+
+// require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+
+// LISTENER
+
+app.listen(PORT, function () {
+    console.log('App listening on PORT ' + PORT);
+});
